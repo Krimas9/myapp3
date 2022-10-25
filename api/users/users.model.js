efc735859fd61a8a1d9820f0f73c8afe26d40192
karima <krimas95@hotmail.com>
2022-10-25 10:29:46 +0200

    rajout de la fonction miniscule pour les entrées emails mi en majuscule

--- api/users/users.model.js
+++ api/users/users.model.js
@@ -3,38 +3,31 @@ const { isEmail } = require("validator");
 // const bcrypt = require("bcrypt");
 
 const userSchema = Schema({
-    name: String,
-    last_name: String,
-    telephone: Number,
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      validate: {
-        validator: isEmail,
-        message: (props) => `${props.value}is not  correct`,
-      },
+  name: String,
+  last_name: String,
+  telephone: Number,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: isEmail,
+      message: (props) => `${props.value}is not  correct`,
     },
-    password: {
-      type: String,
-      required: [true, "password is requires"],
-      minlengh: 8,
-    },
+  },
+  password: {
+    type: String,
+    required: [true, "password is requires"],
+    minlengh: 8,
+  },
+});
 
-// /*userSchema.pre('save', function() {
-//   if (!this.email) {
-//     const error = new Error('mon message')
-//     next(error)
-//   }
-//   next()
-// }) */
-
-// userSchema.pre("save", async function () {
-//   this.email = this.email.toLowerCase();
-// });
+userSchema.pre("save", async function () {
+  this.email = this.email.toLowerCase();
+});
 
 // userSchema.pre("save", async function () {
 //   this.password = await bcrypt.hash(this.password, 10);
-});
+// });
 
 module.exports = model("User", userSchema);
